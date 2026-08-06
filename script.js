e544e7001d160895c60af272f61e9c1c7c29203f
gokulprasath-123 <aizenaizen310@gmail.com>
2026-08-06 20:33:44 +0530

    Add event listeners for pet interaction buttons

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const pet = document.getElementById("pet");
 
+const buttons = document.querySelectorAll("button");
+
+buttons[0].addEventListener("click", () => {
+    alert("🍖 Your pet is happy!");
+});
+
+buttons[1].addEventListener("click", () => {
+    alert("🎮 Your pet is playing!");
+});
+
+buttons[2].addEventListener("click", () => {
+    alert("😴 Your pet is sleeping!");
+});
